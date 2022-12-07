aa0f5b3d2a7906fa55e2844f73262884698694b2
Tolulope <toluadelekesteve@gmail.com>
2022-12-07 18:24:32 +0100

    update

--- src/Components/Dashboard/BarChart.jsx
+++ src/Components/Dashboard/BarChart.jsx
@@ -4,7 +4,7 @@ import { Bar } from "react-chartjs-2";
 
 const BarChart = ({data}) => {
     return (
-        <Bar data={data} options={{scales: {x: {grid: {lineWidth: 0}}}, y: {grid: {offset: false}}, responsive: true,  }} />
+        <Bar data={data} options={{ maintainAspectRatio: false, scales: {x: {grid: {lineWidth: 0}}}, y: {grid: {offset: false}}, responsive: true  }} />
     )
 }
 
